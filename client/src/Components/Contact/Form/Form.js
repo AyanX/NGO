74033166c03aec6fe0517d483b8150aad797c76f
unknown <fearthesorcerer@gmail.com>
2026-01-09 15:02:24 +0300

    contact

--- client/src/Components/Contact/Form/Form.js
+++ client/src/Components/Contact/Form/Form.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import './Form.scss';
+import './Formere.scss';
 
 function ContactForm() {
   const [formData, setFormData] = useState({
